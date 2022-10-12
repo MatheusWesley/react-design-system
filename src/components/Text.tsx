d0bca056d5b684261821649c61634e2c0e3f61ec
Matheus <matheus.tarra@gmail.com>
2022-10-12 20:30:44 -0300

    Comentario inicial

---
+++ src/components/Text.tsx
@@ -0,0 +1,29 @@
+import { clsx } from 'clsx';
+import { Slot } from '@radix-ui/react-slot';
+import { ReactNode } from 'react';
+
+
+export interface TextProps {
+  size?: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean
+}
+
+export function Text({size = 'md', children, asChild}: TextProps) {
+  
+  const CompSlot = asChild ? Slot : 'span'
+  return (
+    <CompSlot
+      className={clsx(
+        'text-gray-100 font-sans',
+        {
+          'text-xs': size === 'sm',
+          'text-sm': size === 'md',
+          'text-md': size === 'lg',
+        }
+      )}
+    >
+      {children}
+    </CompSlot>
+  )
+}
